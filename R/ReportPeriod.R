d70477bdbfa4924e9a06d0b37da5474764a9a29e
jasmyace <jasmyace@gmail.com>
2020-05-25 13:01:29 -0500

    Update so results record which report is which.

--- R/ReportPeriod.R
+++ R/ReportPeriod.R
@@ -5,9 +5,9 @@ ReportPeriod <- function(symbol, CIK, accession.no, accession.no.raw) {
   # accession.no <- 
   # accession.no.raw
      
-  url <- paste0("https://www.sec.gov/Archives/edgar/data/", CIK, "/", 
-                accession.no, "/", accession.no.raw, "-index.htm")
+  url <- paste0("https://www.sec.gov/Archives/edgar/data/", CIK, "/", accession.no, "/", accession.no.raw, "-index.htm")
   search.result <- lapply(url, xml2::read_html)
   report.period <- lapply(search.result, ExtractInfo, ".formGrouping+ .formGrouping .info:nth-child(2)")
+  names(report.period) <- names(accession.no.raw)
   return(report.period)
 }
