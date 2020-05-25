932f6645f06e8f1c84f9624f98a4265fcb6dda0b
jasmyace <jasmyace@gmail.com>
2020-05-25 12:24:09 -0500

    Update to handle more than one period / accession.no.

--- R/ReportPeriod.R
+++ R/ReportPeriod.R
@@ -7,7 +7,7 @@ ReportPeriod <- function(symbol, CIK, accession.no, accession.no.raw) {
      
   url <- paste0("https://www.sec.gov/Archives/edgar/data/", CIK, "/", 
                 accession.no, "/", accession.no.raw, "-index.htm")
-  search.result <- xml2::read_html(url)
-  report.period <- ExtractInfo(search.result, ".formGrouping+ .formGrouping .info:nth-child(2)")
+  search.result <- lapply(url, xml2::read_html)
+  report.period <- lapply(search.result, ExtractInfo, ".formGrouping+ .formGrouping .info:nth-child(2)")
   return(report.period)
 }
