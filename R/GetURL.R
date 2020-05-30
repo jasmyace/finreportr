2b8c5f971a75f243fbf629614429214c13a23224
jasmyace <jasmyace@gmail.com>
2020-05-30 16:14:25 -0500

    Add in quarter annual checks.

--- R/GetURL.R
+++ R/GetURL.R
@@ -20,18 +20,24 @@ GetURL <- function(symbol, year, annual, quarter) {
   report.period <- report.period2
   
   if(year >= 2019){
-    FilingsonEdgark <- edgarWebR::company_filings(x = symbol, type = c("10-K"))
-    findCorrectYeark <- which(lubridate::year(as.Date(FilingsonEdgark$accepted_date)) == as.character(year))  # assumes i'll only ever find one.
-    DocumentsonEdgark <- dplyr::bind_rows(lapply(FilingsonEdgark$href[findCorrectYeark], edgarWebR::filing_documents))
-    inst.urlk <- DocumentsonEdgark[DocumentsonEdgark[5] == 'XML', 4]
-    
-    FilingsonEdgarq <- edgarWebR::company_filings(x = symbol, type = c("10-Q"))
-    findCorrectYearq <- which(lubridate::year(as.Date(FilingsonEdgarq$accepted_date)) == as.character(year) | 
-                                lubridate::year(as.Date(FilingsonEdgarq$accepted_date) + 1) == as.character(year + 1) & months(as.Date(FilingsonEdgarq$accepted_date)) %in% c("January", "February"))  # filings could occur in 1-2020, 2-2020, or more?
-    DocumentsonEdgarq <- dplyr::bind_rows(lapply(FilingsonEdgarq$href[findCorrectYearq], edgarWebR::filing_documents))
-    inst.urlq <- DocumentsonEdgarq[DocumentsonEdgarq[5] == 'XML', 4]
-    
-    inst.url <- c(inst.urlk, inst.urlq)
+    if(annual == TRUE){
+      FilingsonEdgark <- edgarWebR::company_filings(x = symbol, type = c("10-K"))
+      findCorrectYeark <- which(lubridate::year(as.Date(FilingsonEdgark$accepted_date)) == as.character(year))  # assumes i'll only ever find one.
+      DocumentsonEdgark <- dplyr::bind_rows(lapply(FilingsonEdgark$href[findCorrectYeark], edgarWebR::filing_documents))
+      inst.urlk <- DocumentsonEdgark[DocumentsonEdgark[5] == 'XML', 4]
+      inst.url <- inst.urlk
+    } 
+    if(quarter == TRUE){
+      FilingsonEdgarq <- edgarWebR::company_filings(x = symbol, type = c("10-Q"))
+      findCorrectYearq <- which(lubridate::year(as.Date(FilingsonEdgarq$accepted_date)) == as.character(year) | 
+                                  lubridate::year(as.Date(FilingsonEdgarq$accepted_date) + 1) == as.character(year + 1) & months(as.Date(FilingsonEdgarq$accepted_date)) %in% c("January", "February"))  # filings could occur in 1-2020, 2-2020, or more?
+      DocumentsonEdgarq <- dplyr::bind_rows(lapply(FilingsonEdgarq$href[findCorrectYearq], edgarWebR::filing_documents))
+      inst.urlq <- DocumentsonEdgarq[DocumentsonEdgarq[5] == 'XML', 4]
+      inst.url <- inst.urlq
+    }
+    if(annual == TRUE & quarter == TRUE){
+      inst.url <- c(inst.urlk, inst.urlq)
+    }
     names(inst.url) <- names(report.period)
   } else {
     inst.url <- paste0("https://www.sec.gov/Archives/edgar/data/", CIK, "/", accession.no, "/", lower.symbol, "-", report.period, ".xml")
